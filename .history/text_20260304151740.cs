5d8b8d67dc6b2a92f2b3c369da927892bea1b7a7
whitehula <970054928@qq.com>
2026-03-04 15:18:00 +0800

    加入了小白

--- .history/text_20260304151740.cs
+++ .history/text_20260304151740.cs
@@ -7,7 +7,7 @@ static void Main(string[] args)
 {
     List<string> listName = new List<string>()
     listName.Add("李白");
-    listName.Add("小白");
+    listName.Add("John");
     foreach (var item in listName)
     {
         Console.WriteLine(item);
